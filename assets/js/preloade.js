694f027323c589fd54cedf33775a5894fe56499d
acrkdduf <acrkdduf@naver.com>
2019-05-25 21:30:32 +0900

    preloading image changed

--- assets/js/preloade.js
+++ assets/js/preloade.js
@@ -1,4 +1,20 @@
+const preLoader = document.querySelector('.loading-page');
+const fixedNavbar = document.querySelector('#navbar');
+const firstPage = document.querySelector('.onePage-section');
+
 window.addEventListener('load', function() {
-    var preLoader = document.querySelector('.loading-page');
-    preLoader.style.display = 'none';
+    setTimeout(fadeOut, 3000);
 });
+
+function fadeOut() {
+    preLoader.classList.add('hidden');
+    preLoader.addEventListener('animationend', function() {
+        this.style.display = 'none';
+        fadeIn(fixedNavbar);
+        fadeIn(firstPage);
+    })
+}
+
+function fadeIn(element) {
+    element.style.animation = 'fadeIn 1.5s forwards';
+}
