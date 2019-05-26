e322d531c2dff0be23dbddfa687e395f4b6624f2
acrkdduf <acrkdduf@naver.com>
2019-05-26 18:53:09 +0900

    preloader modified

--- assets/js/preloade.js
+++ assets/js/preloade.js
@@ -3,16 +3,24 @@ const fixedNavbar = document.querySelector('#navbar');
 const firstPage = document.querySelector('.onePage-section');
 
 window.addEventListener('load', function() {
-    setTimeout(fadeOut, 3000);
+    setTimeout(afterPreload, 3000);
 });
 
-function fadeOut() {
+function afterPreload() {
     preLoader.classList.add('hidden');
-    preLoader.addEventListener('animationend', function() {
+    fadeOut(preLoader);
+}
+
+function fadeOut(element) {
+    element.addEventListener('animationend', function() {
         this.style.display = 'none';
         fadeIn(fixedNavbar);
         fadeIn(firstPage);
-    })
+    });
+}
+
+function noneScreen(element) {
+    element.style.display = 'none';
 }
 
 function fadeIn(element) {
