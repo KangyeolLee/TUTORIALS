4ff2ef9331cc37efdd82e6a23a5753a7d4116ef0
acrkdduf <acrkdduf@naver.com>
2019-06-03 18:12:55 +0900

    왜안되노

--- assets/js/preloade.js
+++ assets/js/preloade.js
@@ -2,6 +2,7 @@ const preLoader = document.querySelector('.loading-page');
 const progressContainer = document.querySelector('.progress-container');
 const progressbar = document.querySelector('.progress-bar');
 const preLoaderFire = document.querySelector('.page-preloader-wrapper .fire');
+const preLoaderFlame = document.querySelector('.page-preloader-wrapper .fire .flames .flame');
 const preLoaderNav = document.querySelector('#nav-preloader');
 const fixedNavbar = document.querySelector('#navbar');
 const firstOnePageSection = document.querySelector('#banner');
@@ -25,6 +26,7 @@ function afterPreload() {
     progressContainer.addEventListener('animationend', function() {
         preLoader.classList.add('hidden');
         preLoaderFire.classList.add('hidden');
+        preLoaderFlame.classList.add('hidden');
         preLoaderNav.classList.add('hidden');
 
         preLoader.addEventListener('animationend', function() {
