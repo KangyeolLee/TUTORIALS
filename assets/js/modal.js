dba5393f768f8a67d37d0f8280c82ba909792344
95hwang <95chris0319@gmail.com>
2019-06-10 11:36:00 +0900

    emailInputForm charCounting

--- assets/js/modal.js
+++
@@ -1,7 +0,0 @@
-const inputMessage = document.querySelector('#inputMessage');
-const modalMessage = document.querySelector('#modalMessage');
-const message = document.querySelector('#message');
-inputMessage.addEventListener('click', () => {
-  const m = modalMessage.value;
-  message.value = m;
-})
