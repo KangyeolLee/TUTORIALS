e7d48ee4105d48569e9d20416f91734ea0d7ca6e
95hwang <95chris0319@gmail.com>
2019-06-12 15:26:11 +0900

    emailModal fixed

--- assets/js/emailForm.js
+++ assets/js/emailForm.js
@@ -1,17 +1,22 @@
 const inputMessage = document.querySelector('#inputMessage');
 const modalMessage = document.querySelector('#modalMessage');
 const message = document.querySelector('#message');
-
 inputMessage.addEventListener('click', () => {
   const m = modalMessage.value;
   message.value = m;
 });
 
+document.addEventListener('DOMContentLoaded', function(){
+  modalTrigger();
+  window.addEventListener('resize', ()=>{
+    modalTrigger();
+  });
+});
 
 //const textarea = document.querySelector('#modalMessage');
 modalMessage.addEventListener('keyup', ()=>{
   const text = modalMessage.value;
-  const length = text.length; 
+  const length = text.length;
   charlength = document.querySelector(`div[charlength]`);
   const value = charlength.getAttribute('charlength');
   document.getElementById('counting').innerHTML= length + "/"+ value;
@@ -20,3 +25,14 @@ modalMessage.addEventListener('keyup', ()=>{
     alert("입력범위 초과");
   }
 });
+
+function modalTrigger(){
+  const windowSize = document.body.clientWidth;
+  if(windowSize > 600){
+    message.classList.remove("modal-trigger");
+    console.log(windowSize);
+  }
+  else{
+    message.classList.add("modal-trigger");
+  }
+}
