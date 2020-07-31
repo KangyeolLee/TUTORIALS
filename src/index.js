c0776316c39f430278379aa92d118e5b33948abd
KG LEE <acrkdduf@naver.com>
2020-08-01 01:04:51 +0900

    basic react hooks with redux-middleware tutorial

--- src/index.js
+++ src/index.js
@@ -1,14 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import rootReducer, { rootSaga } from "./modules/index";
+import { composeWithDevTools } from "redux-devtools-extension";
+// import myLogger from "./middlewares/myLogger";
+import logger from "redux-logger";
+import ReduxThunk from "redux-thunk";
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import createSagaMiddleware from "redux-saga";
+
+const customHistory = createBrowserHistory();
+const sagaMiddleware = createSagaMiddleware({
+  context: {
+    history: customHistory,
+  },
+});
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(
+      ReduxThunk.withExtraArgument({ history: customHistory }),
+      sagaMiddleware,
+      logger
+    )
+  )
+);
+
+sagaMiddleware.run(rootSaga); // 루트사가 실행
+// 주의: 스토어 생성이 완료된 후에 루트사가를 실행해야 함
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router history={customHistory}>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
+  </Router>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
