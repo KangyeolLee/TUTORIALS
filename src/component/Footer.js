9bf69c2f48a363818c6725c53e447d9489ada59e
KG LEE <acrkdduf@naver.com>
2021-03-25 22:22:14 +0900

    for backup

---
+++ src/component/Footer.js
@@ -0,0 +1,9 @@
+import { Divider } from 'semantic-ui-react'
+export default function Footer() {
+  return (
+    <>
+      <Divider />
+      <div>Copyright #KG#. All rights reserved.</div>
+    </>
+  )
+}
