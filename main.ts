c8e7dcf6a28b42142fa14836e2a06de83b57ca72
KG <acrkdduf@naver.com>
2021-05-04 20:16:19 +0900

    typeschecking null & undefined

--- main.ts
+++ main.ts
@@ -37,3 +37,15 @@ console.log(Week);
 console.log(Week[0]);
 console.log(Week.Sun);
 console.log(Week["Sun"]);
+
+// --------------- null & undefined --------------- //
+/**
+ * 기본적으로 null 과 undefined 는 모든 타입의 하위 타입 => 서로 할당도 가능
+ * strictNullChecks: true 옵션을 통해 막을 수 있음
+ * 그러나 void 에는 undefined 할당 가능
+ */
+let num: number = undefined;
+let str: string = null;
+let und: undefined = null;
+let nul: null = undefined;
+let voi: void = undefined;
