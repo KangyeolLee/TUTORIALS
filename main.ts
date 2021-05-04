c88d86a1aca008abec5c1f796f1436296a116736
KG <acrkdduf@naver.com>
2021-05-04 20:10:24 +0900

    enum examples

--- main.ts
+++ main.ts
@@ -6,6 +6,8 @@ const sum: number = add(1, 2);
 
 console.log(sum);
 
+// --------------- tuple --------------- //
+
 let tuple: [string, number];
 tuple = ["a", 1];
 
@@ -18,3 +20,20 @@ console.log(tuple);
 tuple.push(3);
 tuple.push("abc");
 console.log(tuple);
+
+// --------------- enum --------------- //
+
+enum Week {
+  Sun,
+  Mon,
+  Tue,
+  Wed,
+  Thu,
+  Fri,
+  Sat,
+}
+
+console.log(Week);
+console.log(Week[0]);
+console.log(Week.Sun);
+console.log(Week["Sun"]);
