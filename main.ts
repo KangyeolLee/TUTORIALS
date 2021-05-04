c8f2456d2180a77c95d20876755fc76946a97ede
KG <acrkdduf@naver.com>
2021-05-04 20:07:27 +0900

    tuple examples

--- main.ts
+++ main.ts
@@ -5,3 +5,16 @@ function add(a: number, b: number) {
 const sum: number = add(1, 2);
 
 console.log(sum);
+
+let tuple: [string, number];
+tuple = ["a", 1];
+
+console.log(tuple);
+
+tuple = ["b", 2];
+
+console.log(tuple);
+
+tuple.push(3);
+tuple.push("abc");
+console.log(tuple);
