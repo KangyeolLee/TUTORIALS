026688b26c11e0c0968659a6914adbc205b1617a
KG <acrkdduf@naver.com>
2021-05-19 19:32:08 +0900

    mocha test lib tutorial

---
+++ atm.js
@@ -0,0 +1,19 @@
+var totalDeposit = 0;
+var Account = function (deposit) {
+  totalDeposit = deposit;
+};
+
+Account.prototype.getAccount = function () {
+  return totalDeposit;
+};
+
+Account.prototype.withDraw = function (money) {
+  this.money = money;
+  totalDeposit -= money;
+};
+
+Account.prototype.AtmRemainingCash = function () {
+  return this.money;
+};
+
+module.exports = Account;
