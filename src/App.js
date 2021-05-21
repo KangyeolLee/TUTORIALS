5723f22ceee39910bfba0b65ee8f478b2d9a1603
KG <acrkdduf@naver.com>
2021-05-22 05:12:05 +0900

    connect kakao payment module system with proxy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { useState, useEffect } from "react";
 import axios from "axios";
 import Loading from "./Loading";
+import kakaopayConfig from "./kakaopay.config";
 
 function App() {
+  const { params } = kakaopayConfig;
+  // const [nextUrl, setNextUrl] = useState();
+
+  useEffect(() => {
+    const postKakaopay = async () => {
+      const data = await axios.post("/v1/payment/ready", null, {
+        params,
+        headers: {
+          Authorization: "KakaoAK 17f6c6ec457977e41c71d0efed5ce335",
+          "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+        },
+      });
+      console.log(data);
+    };
+    postKakaopay();
+  }, []);
+
   return (
     <div className="App">
       <Loading />
