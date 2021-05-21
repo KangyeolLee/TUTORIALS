1e7d3a2856fa9948ea52bf7f4491958432b1392e
KG <acrkdduf@naver.com>
2021-05-22 03:46:53 +0900

    add config file for kakaopay

---
+++ src/kakaopay.config.js
@@ -0,0 +1,19 @@
+const config = {
+  next_redirect_pc_url: "",
+  tid: "",
+  params: {
+    cid: "TC0ONETIME",
+    partner_order_id: "partner_order_id",
+    partner_user_id: "partner_user_id",
+    item_name: "초코파이",
+    quantity: 1,
+    total_amount: 2200,
+    vat_amount: 200,
+    tax_free_amount: 0,
+    approval_url: "http://localhost:3000/",
+    fail_url: "http://localhost:3000/",
+    cancel_url: "http://localhost:3000/",
+  },
+};
+
+export default config;
