53d53f8f64c9ac6542670ba8db5e99433f0202c8
KG <acrkdduf@naver.com>
2021-06-03 03:39:06 +0900

    optimize for mobile

--- components/layouts/PreviewNovelWrapper.tsx
+++ components/layouts/PreviewNovelWrapper.tsx
@@ -2,7 +2,7 @@ import PreviewNovel from "./PreviewNovel";
 
 const PreviewNovelWrapper = () => {
   return (
-    <section className="grid grid-cols-2 lg:grid-cols-4 md:grid-cols-3 gap-10 my-10">
+    <section className="grid grid-cols-1 mob:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 my-10">
       <PreviewNovel />
       <PreviewNovel />
       <PreviewNovel />
