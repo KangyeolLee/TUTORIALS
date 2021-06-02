3e41ad950852573882bbdd125de31b6e4de52097
KG <acrkdduf@naver.com>
2021-06-02 15:40:19 +0900

    add preview novel wrapper component

---
+++ components/layouts/PreviewNovelWrapper.tsx
@@ -0,0 +1,20 @@
+import PreviewNovel from "./PreviewNovel";
+
+const PreviewNovelWrapper = () => {
+  return (
+    <section className="grid grid-cols-2 lg:grid-cols-4 md:grid-cols-3 gap-10 my-10">
+      <PreviewNovel />
+      <PreviewNovel />
+      <PreviewNovel />
+      <PreviewNovel />
+      <PreviewNovel />
+      <PreviewNovel />
+      <PreviewNovel />
+      <PreviewNovel />
+      <PreviewNovel />
+      <PreviewNovel />
+    </section>
+  );
+};
+
+export default PreviewNovelWrapper;
