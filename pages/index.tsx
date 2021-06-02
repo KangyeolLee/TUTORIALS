d43fecb29ad87caa4953145b7e3d3e89a0d9c6a9
KG <acrkdduf@naver.com>
2021-06-03 03:39:33 +0900

    optimize for mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <Banner />
-      <div className="sm:container sm:mx-auto md:px-10">
+      <div className="container px-5 md:px-10 mx-auto">
         <h1 className="text-3xl my-10 font-bold">오늘의 연재</h1>
         <PreviewNovelWrapper />
       </div>
