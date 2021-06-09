5d6d6c90f76cf39749140a58006e5f1ed2efa22b
KG <acrkdduf@naver.com>
2021-06-09 20:55:09 +0900

    modularize with Component Unit into Contributor

--- components/layouts/Contributors.tsx
+++ components/layouts/Contributors.tsx
@@ -1,40 +1,24 @@
 import React from "react";
 import Image from "next/image";
+import Contributor from "./Contributor";
 
 const Contributors = () => {
   return (
     <>
       <h1 className="mt-14 mb-2 text-2xl font-bold">기여자 랭킹</h1>
-      <div className="border rounded-md p-6 mob:p-10">
-        <div className="flex gap-3 mob:gap-5 my-2">
-          <Image src="/img/gold.svg" alt="gold medal" width={30} height={30} />
-          <h1 className="font-bold">nickname</h1>
-          <Image src="/img/vote.svg" alt="vote" width={30} height={30} />
-          <p>21회</p>
-        </div>
+      <div className="border rounded-md p-6 sm:p-8">
+        <div className="grid sm:gap-5 grid-flow-row sm:grid-cols-2">
+          <div className="grid grid-rows-5">
+            <Contributor nickname={"오늘의소설"} count={21} rank={1} />
+            <Contributor nickname={"내일의소설"} count={15} rank={2} />
+            <Contributor nickname={"어제의소설"} count={11} rank={3} />
+            <Contributor nickname={"모레의소설"} count={9} rank={4} />
+            <Contributor nickname={"이레의소설"} count={8} rank={5} />
+          </div>
 
-        <div className="flex gap-3 mob:gap-5 my-2">
-          <Image
-            src="/img/silver.svg"
-            alt="silver medal"
-            width={30}
-            height={30}
-          />
-          <h1 className="font-bold">nickname</h1>
-          <Image src="/img/vote.svg" alt="vote" width={30} height={30} />
-          <p>11회</p>
-        </div>
-
-        <div className="flex gap-3 mob:gap-5 my-2">
-          <Image
-            src="/img/bronze.svg"
-            alt="bronze medal"
-            width={30}
-            height={30}
-          />
-          <h1 className="font-bold">nickname</h1>
-          <Image src="/img/vote.svg" alt="vote" width={30} height={30} />
-          <p>10회</p>
+          <div className="grid grid-rows-5">
+            <Contributor nickname={"나는야소설"} count={5} rank={6} />
+          </div>
         </div>
       </div>
     </>
