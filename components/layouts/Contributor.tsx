c543b631c8164d2e00fe6e3249ca20f53b1688b0
KG <acrkdduf@naver.com>
2021-06-09 20:54:19 +0900

    add Component for Contributor

---
+++ components/layouts/Contributor.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Image from "next/image";
+
+interface Props {
+  nickname: string;
+  count: number;
+  rank: number;
+}
+
+const Contributor = ({ nickname, count, rank }: Props) => {
+  const medal = rank === 1 ? "gold" : rank === 2 ? "silver" : "bronze";
+  return (
+    <div className="flex justify-between gap-2 sm:gap-4 my-2 cursor-pointer hover:bg-gray-200 border rounded-md p-3">
+      <div className="flex gap-2 md:gap-4">
+        <Image
+          src={`/img/${medal}.svg`}
+          alt="gold medal"
+          width={30}
+          height={30}
+        />
+        <h1>
+          <strong>{nickname}</strong> 님
+        </h1>
+      </div>
+
+      <div className="flex gap-2 md:gap-4">
+        <Image src="/img/vote.svg" alt="vote" width={30} height={30} />
+        <p>{count}회</p>
+      </div>
+    </div>
+  );
+};
+
+export default Contributor;
