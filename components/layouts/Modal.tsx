5f513a79b7e8955d72b8ed207dba34fa6b44a715
KG <acrkdduf@naver.com>
2021-06-09 18:54:03 +0900

    set max-width in modal block for mobile optimize

--- components/layouts/Modal.tsx
+++ components/layouts/Modal.tsx
@@ -6,8 +6,8 @@ type Props = {
 
 const Modal: VFC<Props> = ({ handleSubmitBtn }) => {
   return (
-    <div className="fixed flex flex-col justify-center top-0 left-0 w-full h-full bg-black bg-opacity-60 z-50">
-      <div className="flex flex-col gap-5 bg-white p-10 m-3 sm:m-16 lg:m-40 xl:m-60 rounded-3xl">
+    <div className="fixed flex flex-col items-center justify-center p-6 top-0 left-0 w-full h-full bg-black bg-opacity-60 z-50">
+      <div className="flex flex-col gap-5 bg-white w-full max-w-3xl p-10 rounded-3xl">
         <h1 className="font-bold text-2xl">CCC님의 코멘트</h1>
         <input
           placeholder="자유롭게 의견을 남겨보세요"
