fc09fdb89edd0d51b5b443fb6f93247fda1eff61
KG <acrkdduf@naver.com>
2021-06-10 17:44:28 +0900

    update banner image url into another src

--- components/layouts/Banner.tsx
+++ components/layouts/Banner.tsx
@@ -5,8 +5,8 @@ const Banner = () => {
   return (
     <section className="h-72 sm:h-96 relative z-0">
       <Image
-        src="https://novelpia.com/img/new/event/money_copy1/pc/event_board.png"
-        alt=""
+        src="https://clayworks.space/wp-content/uploads/2013/10/contact-banner-image-scaled.jpg"
+        alt="배너이미지"
         layout="fill"
         objectFit="cover"
       />
