5b35fd67b693eaf4ca9d5adf5ef0891c378cd454
KG <acrkdduf@naver.com>
2021-06-11 19:04:58 +0900

    re-organize with seperated Components and dummydata

--- pages/novel/[id]/index.tsx
+++ pages/novel/[id]/index.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import NovelList from "@components/layouts/NovelList";
 import NovelDescription from "@components/layouts/NovelDescription";
 import Contributors from "@components/layouts/Contributors";
 import UserModal from "@components/layouts/UserModal";
 import NovelListWrapper from "@components/layouts/NovelListWrapper";
+import { dummyContributors, dummyNovels } from "@dummy/dummyData";
 
 const NovelDetail = () => {
   const [modalStatus, setModalStatus] = useState(false);
@@ -16,9 +16,12 @@ const NovelDetail = () => {
     <section className="sm:container px-5 md:px-10 mx-auto my-16">
       <NovelDescription title="이것은 소설의 제목입니다" writer="Gian95" />
 
-      <Contributors openUserModal={openUserModal} />
+      <Contributors
+        openUserModal={openUserModal}
+        contributors={dummyContributors}
+      />
 
-      <NovelListWrapper list={dummyData} />
+      <NovelListWrapper list={dummyNovels} />
 
       {modalStatus && <UserModal handleSubmitBtn={openUserModal} />}
     </section>
@@ -26,13 +29,3 @@ const NovelDetail = () => {
 };
 
 export default NovelDetail;
-
-const dummyData = [
-  { num: 1, title: "프롤로그" },
-  { num: 2, title: "가자 디지몬 친구들" },
-  { num: 3, title: "동해물과" },
-  { num: 4, title: "백두산이" },
-  { num: 5, title: "마르고 닳도록" },
-  { num: 6, title: "하나님이 보우하사" },
-  { num: 7, title: "우리나라 만세" },
-];
