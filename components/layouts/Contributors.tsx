5cb0dacc8d1a573cd04092f99a4f12c509857316
KG <acrkdduf@naver.com>
2021-06-11 19:05:55 +0900

    re-organize the origin structures due to connection to dummydata&type

--- components/layouts/Contributors.tsx
+++ components/layouts/Contributors.tsx
@@ -1,60 +1,52 @@
+import { ContributorType } from "@dummy/dummyType";
 import React, { VFC } from "react";
 import Contributor from "./Contributor";
 
 interface Props {
   openUserModal: () => void;
+  contributors: ContributorType[];
 }
 
-const Contributors: VFC<Props> = ({ openUserModal }) => {
+const LIMIT_PER_CONTRIBUTOR = 5;
+
+const Contributors: VFC<Props> = ({ openUserModal, contributors }) => {
   return (
-    <>
+    <section>
       <h1 className="mt-14 mb-2 text-2xl font-bold">기여자 랭킹</h1>
-      <div className="border rounded-md p-6 sm:p-8">
+      <article className="border rounded-md p-6 sm:p-8">
         <div className="grid sm:gap-5 grid-flow-row sm:grid-cols-2">
           <div className="grid grid-rows-5">
-            <Contributor
-              openUserModal={openUserModal}
-              nickname={"오늘의소설"}
-              count={21}
-              rank={1}
-            />
-            <Contributor
-              openUserModal={openUserModal}
-              nickname={"내일의소설"}
-              count={15}
-              rank={2}
-            />
-            <Contributor
-              openUserModal={openUserModal}
-              nickname={"어제의소설"}
-              count={11}
-              rank={3}
-            />
-            <Contributor
-              openUserModal={openUserModal}
-              nickname={"모레의소설"}
-              count={9}
-              rank={4}
-            />
-            <Contributor
-              openUserModal={openUserModal}
-              nickname={"이레의소설"}
-              count={8}
-              rank={5}
-            />
+            {contributors.length &&
+              contributors
+                .slice(0, LIMIT_PER_CONTRIBUTOR)
+                .map((contributor) => (
+                  <Contributor
+                    key={contributor.id}
+                    nickname={contributor.nickname}
+                    count={contributor.count}
+                    rank={contributor.rank}
+                    openUserModal={openUserModal}
+                  />
+                ))}
           </div>
 
           <div className="grid grid-rows-5">
-            <Contributor
-              openUserModal={openUserModal}
-              nickname={"나는야소설"}
-              count={5}
-              rank={6}
-            />
+            {contributors.length &&
+              contributors
+                .slice(LIMIT_PER_CONTRIBUTOR)
+                .map((contributor) => (
+                  <Contributor
+                    key={contributor.id}
+                    nickname={contributor.nickname}
+                    count={contributor.count}
+                    rank={contributor.rank}
+                    openUserModal={openUserModal}
+                  />
+                ))}
           </div>
         </div>
-      </div>
-    </>
+      </article>
+    </section>
   );
 };
 
