a7e19a9889919d1c9e9470d02cbdb5e163f3ab55
KG <acrkdduf@naver.com>
2021-06-12 16:33:29 +0900

    add unique key in displaying interable mapping Components

--- components/layouts/NovelListWrapper.tsx
+++ components/layouts/NovelListWrapper.tsx
@@ -12,7 +12,7 @@ const NovelListWrapper: VFC<Props> = ({ list }) => {
       <h1 className="mt-14 mb-2 text-2xl font-bold">회차 목록</h1>
       <article className="divide-y divide-light-gray-500">
         {list.map((novel) => (
-          <NovelList num={novel.num} title={novel.title} />
+          <NovelList key={novel.id} num={novel.num} title={novel.title} />
         ))}
       </article>
     </section>
