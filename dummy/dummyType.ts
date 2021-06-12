f067d743fb374b934f325d53391b47d600be6d33
KG <acrkdduf@naver.com>
2021-06-12 16:39:36 +0900

    add dummy type for temporal rendering and matching types in Components

---
+++ dummy/dummyType.ts
@@ -0,0 +1,12 @@
+export type ContributorType = {
+  id: string;
+  nickname: string;
+  count: number;
+  rank: number;
+};
+
+export type NovelType = {
+  id: string;
+  num: number;
+  title: string;
+};
