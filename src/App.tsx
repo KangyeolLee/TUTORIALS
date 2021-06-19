ff435fe9ce4ce45095832d67040dc405dce902f3
KG <acrkdduf@naver.com>
2021-06-20 00:57:44 +0900

    add Header at top of application

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { Center, ChakraProvider } from "@chakra-ui/react";
+import Tabs from "@components/Tabs";
+
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <ChakraProvider>
+      <Center p={3} bg="purple.600" color="#fff">
+        영웅시대
+      </Center>
+      <Tabs />
+    </ChakraProvider>
+  );
 }
 
 export default App;
