214020fbe644f795bf6b3bb37c1dcf4518411731
KG <acrkdduf@naver.com>
2021-06-21 20:34:36 +0900

    add dummy data for youtube videos for testing Router connection

---
+++ src/dummyData/youtubes.ts
@@ -0,0 +1,31 @@
+import { v4 as uuid } from "uuid";
+
+export const dummyYoutube_news = [
+  {
+    id: uuid(),
+    title:
+      "임영웅 대구콘서트 마지막날 사연읽던중 발생한 상황에 팬들 난리났다?왜? 직접 지목한 관객? 조용필 노래부른 진짜이유? 미스터트롯 콘서트 뽕뉴스",
+    video: "Yu5panwoi8A",
+    thumbnail:
+      "https://i.ytimg.com/vi/Yu5panwoi8A/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC5aBzbI3uMFrvyyDVffeQ_qJIAnA",
+    time: "12:20",
+  },
+  {
+    id: uuid(),
+    title:
+      "가수 임영웅 생일!? 현직 유명 무속인이 보는 임영웅의 놀라운 사주와 내년에 벌어질 일들..그의 숨겨진 생각과 성격들",
+    video: "GwZWwST_jq4",
+    thumbnail:
+      "https://i.ytimg.com/vi/GwZWwST_jq4/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCM030svvevVG5Jb0bNlHPW_GOzaw",
+    time: "11:10",
+  },
+  {
+    id: uuid(),
+    title:
+      "연구위원 ***님의 감동의 말씀 임영웅 이모저모 #임영웅유튜브 #LimYoungWoong #임영웅사랑의콜센타",
+    video: "KW_yJ8pxNhQ",
+    thumbnail:
+      "https://i.ytimg.com/vi/KW_yJ8pxNhQ/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBquSM68R1mqQnr2sysD2VdFHYFjg",
+    time: "4:59",
+  },
+];
