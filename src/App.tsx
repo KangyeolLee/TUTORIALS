1bb8cdafae04f38469cfb469ed54a530771e7028
KG <acrkdduf@naver.com>
2021-06-21 20:32:40 +0900

    add Router Component at sub-root

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import { Center, ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Tabs from "@components/Tabs";
+import YoutubeVideo from "@components/YoutubeVideo";
 
 function App() {
   return (
     <ChakraProvider>
-      <Center p={3} bg="purple.600" color="#fff">
-        영웅시대
-      </Center>
-      <Tabs />
+      <Router>
+        <Center p={3} bg="purple.600" color="#fff">
+          영웅시대
+        </Center>
+
+        <Switch>
+          <Route exact path="/" component={Tabs} />
+          <Route path="/youtube/:id" component={YoutubeVideo} />
+        </Switch>
+      </Router>
     </ChakraProvider>
   );
 }
