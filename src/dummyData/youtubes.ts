d81f82b823b97e3b0f9222c39071d5c086608394
KG <acrkdduf@naver.com>
2021-06-22 21:14:03 +0900

    add more dummy data types for selecting category and appopriate rendering

--- src/dummyData/youtubes.ts
+++ src/dummyData/youtubes.ts
@@ -1,8 +1,6 @@
-import { v4 as uuid } from "uuid";
-
 export const dummyYoutube_news = [
   {
-    id: uuid(),
+    id: "Yu5panwoi8A",
     title:
       "임영웅 대구콘서트 마지막날 사연읽던중 발생한 상황에 팬들 난리났다?왜? 직접 지목한 관객? 조용필 노래부른 진짜이유? 미스터트롯 콘서트 뽕뉴스",
     video: "Yu5panwoi8A",
@@ -11,7 +9,7 @@ export const dummyYoutube_news = [
     time: "12:20",
   },
   {
-    id: uuid(),
+    id: "GwZWwST_jq4",
     title:
       "가수 임영웅 생일!? 현직 유명 무속인이 보는 임영웅의 놀라운 사주와 내년에 벌어질 일들..그의 숨겨진 생각과 성격들",
     video: "GwZWwST_jq4",
@@ -20,12 +18,142 @@ export const dummyYoutube_news = [
     time: "11:10",
   },
   {
-    id: uuid(),
+    id: "KW_yJ8pxNhQ",
     title:
       "연구위원 ***님의 감동의 말씀 임영웅 이모저모 #임영웅유튜브 #LimYoungWoong #임영웅사랑의콜센타",
     video: "KW_yJ8pxNhQ",
     thumbnail:
       "https://i.ytimg.com/vi/KW_yJ8pxNhQ/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBquSM68R1mqQnr2sysD2VdFHYFjg",
     time: "4:59",
+  },
+  {
+    id: "2YNeggiR4yU",
+    title:
+      "설운도 두 번째 곡? 아직 이야기 할 단계 아니다  윤종신 프로듀서 오디션 성공의 대명사로 임영웅 언급",
+    video: "2YNeggiR4yU",
+    thumbnail:
+      "https://i.ytimg.com/vi/2YNeggiR4yU/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC9CoUSIL60C5DYn4WF52CJmfmihg",
+    time: "8:57",
+  },
+  {
+    id: "mg5NBJCWfYM",
+    title:
+      "임영웅 또 3천만원....이번에는 TV조선 자회사 티조컬처앤컨텐츠와 기부",
+    video: "mg5NBJCWfYM",
+    thumbnail:
+      "https://i.ytimg.com/vi/mg5NBJCWfYM/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBacsiWJsKpd3vdsCodVgHXRunw2Q",
+    time: "2:34",
+  },
+  {
+    id: "Jqs5WEDofBE",
+    title:
+      "[단독] 배우 최수종의 컨디션이 나빠진 소식을 듣고 임영웅과 영탁이 남양주 병원으로 달려갔다!? 임영웅은 아내 최수종이 눈물을 흘리게한 뭐라고? 하희라 임영웅에게 사과한 이유?",
+    video: "Jqs5WEDofBE",
+    thumbnail:
+      "https://i.ytimg.com/vi/Jqs5WEDofBE/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDzT0PmIrBR90Ci7QK0Hud2dnYE-Q",
+    time: "3:14",
+  },
+  {
+    id: "skI0mtypHKU",
+    title:
+      "맘껏 자랑해도 좋습니다 믿을 수 없는 기록 멜론 랩& 힙합 차트 래퍼웅 TOP100 진입 (핫차트 TOP5)",
+    video: "skI0mtypHKU",
+    thumbnail:
+      "https://i.ytimg.com/vi/skI0mtypHKU/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDFJaoNjeiJNn15UF2PhqOWF0_S2Q",
+    time: "8:22",
+  },
+  {
+    id: "alHrsI1ZLqk",
+    title: `(핫) 임영웅 대구에 있다는 소식을 접한 이찬원 아버지는 콘서트에 밥을 가져다줬다. "우리 아들 살 빠졌니"... 감동의 눈물을 흘리며 찬원 아버지를 안았다`,
+    video: "alHrsI1ZLqk",
+    thumbnail:
+      "https://i.ytimg.com/vi/alHrsI1ZLqk/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB83VkJ_nLXskF6KDg8xnC9s5itMg",
+    time: "2:40",
+  },
+];
+
+export const dummyYoutube_songs = [
+  {
+    id: "z2IjyUZoxJg",
+    title: `임영웅 노래모음 14곡을 광고없이듣기 - 임영웅 마음의 위로가 되는 노래모음 ♥ 100분 연속듣기 PLAYLIST 어느 60대 노부부이야기, 사랑이 이런건가요, 오래된 노래`,
+    video: "z2IjyUZoxJg",
+    thumbnail:
+      "https://i.ytimg.com/vi/z2IjyUZoxJg/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDTRM3TLyioF8917AAX9517xhObUw",
+    time: "28:26",
+  },
+  {
+    id: "fgnAnpM253Y",
+    title: `사랑의 콜센타 임영웅 노래모음 ⭐2시간 연속듣기⭐`,
+    video: "fgnAnpM253Y",
+    thumbnail:
+      "https://i.ytimg.com/vi/fgnAnpM253Y/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAeTUr9yjIhTUNRbXtVNRkPnMUsJg",
+    time: "1:46:19",
+  },
+  {
+    id: "AnOGzx1vuhM",
+    title: `임영웅 노래모음 14곡을 광고없이듣기 - 임영웅 마음의 위로가 되는 노래모음 ♥ 100분 연속듣기 PLAYLIST 어느 60대 노부부이야기, 사랑이 이런건가요, 오래된 노래`,
+    video: "AnOGzx1vuhM",
+    thumbnail:
+      "https://i.ytimg.com/vi/AnOGzx1vuhM/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB6MDJRuj02r09PdvIZussfQ7kC7A",
+    time: "1:37:42",
+  },
+  {
+    id: "ssu3E7eDtI0",
+    title: `임영웅 노래모음 14곡을 광고없이듣기 🍒임영웅 마음의 위로가 되는 노래모음 ♥ 100분 연속듣기 PLAYLIST 어느 60대 노부부이야기, 사랑이 이런건가요, 오래된`,
+    video: "ssu3E7eDtI0",
+    thumbnail:
+      "https://i.ytimg.com/vi/ssu3E7eDtI0/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDRFsgGA3DR8op2enoJbOIj4i3vFw",
+    time: "1:24:53",
+  },
+  {
+    id: "RfafwqLc0H0",
+    title: `임영웅 마음의 위로가 되는 노래모음♥ 100분 연속듣기 PLAYLIST 어느 60대 노부부이야기, 사랑이 이런건가요, 오래된 노래, 사랑의콜센타, 뽕숭아학당, 미스터트롯`,
+    video: "RfafwqLc0H0",
+    thumbnail:
+      "https://i.ytimg.com/vi/RfafwqLc0H0/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCSZem8EY6Pc4ZN-osTmts9qBtUZQ",
+    time: "1:14:31",
+  },
+  {
+    id: "OQk1MayqSU0",
+    title: `미스터트롯 임영웅 곡모음 ⭐12곡 연속듣기⭐`,
+    video: "OQk1MayqSU0",
+    thumbnail:
+      "https://i.ytimg.com/vi/OQk1MayqSU0/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDLzafemf5orfdZldPm8thYT8tKNg",
+    time: "41:18",
+  },
+];
+
+export const dummyYoutube_officials = [
+  {
+    id: "rTv7k_FiFaI",
+    title: `[임영웅] 새콤달콤 과즙미(?) 히어로의 생일! 코 끝에선 i셔🍋~ 입 안에서도 i셔😝~ (ENG)`,
+    video: "rTv7k_FiFaI",
+    thumbnail:
+      "https://i.ytimg.com/vi/rTv7k_FiFaI/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBzb79aqV50WC0yFTes3YCgUwLrAQ",
+    time: "16:50",
+  },
+  {
+    id: "KZMAvB1unQs",
+    title: `[임영웅] 레고를 조립했습니다. 그런데 이제 ASMR을 곁들인.. (Feat. 깜짝 전화연결) (ENG)`,
+    video: "KZMAvB1unQs",
+    thumbnail:
+      "https://i.ytimg.com/vi/KZMAvB1unQs/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAeOtFqnf81QNI7F7yXrXQh8qtJXQ",
+    time: "18:35",
+  },
+  {
+    id: "pZ2TuT7IW5A",
+    title: `[임영웅] 롤링스톤 코리아 창간호의 비하인드 (ENG)`,
+    video: "pZ2TuT7IW5A",
+    thumbnail:
+      "https://i.ytimg.com/vi/pZ2TuT7IW5A/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBdZhzQqDiTg5dvZM77rpkoe9ktKA",
+    time: "4:42",
+  },
+  {
+    id: "7dOzmxVAtRU",
+    title: `[임영웅] 치과의사가 된 임영웅!? | 덴티스 광고 비하인드 (ENG)`,
+    video: "7dOzmxVAtRU",
+    thumbnail:
+      "https://i.ytimg.com/vi/7dOzmxVAtRU/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBzAaQ8Yq5Ha75K6Gk_RaOhtuotrA",
+    time: "4:57",
   },
 ];
