c6a9d68df7bd50850b3dc79d918dcebfbfbd6660
KG <acrkdduf@naver.com>
2021-06-22 21:13:04 +0900

    re-organize the origin structures for sticky Header in the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import { Center, ChakraProvider } from "@chakra-ui/react";
+import { Box, ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Tabs from "@components/Tabs";
 import YoutubeVideo from "@components/YoutubeVideo";
+import Header from "@components/Header";
 
 function App() {
   return (
     <ChakraProvider>
       <Router>
-        <Center p={3} bg="purple.600" color="#fff">
-          영웅시대
-        </Center>
-
-        <Switch>
-          <Route exact path="/" component={Tabs} />
-          <Route path="/youtube/:id" component={YoutubeVideo} />
-        </Switch>
+        <Header />
+        <Box as="main" pt="48px">
+          <Switch>
+            <Route exact path="/" component={Tabs} />
+            <Route path="/youtube/:id" component={YoutubeVideo} />
+          </Switch>
+        </Box>
       </Router>
     </ChakraProvider>
   );
