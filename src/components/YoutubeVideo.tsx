ca50d9367bdc6c69cbcce73465f10b382a84c800
KG <acrkdduf@naver.com>
2021-06-22 21:18:57 +0900

    add time printing Box in the bottom-middle of the Image Thumbnail

--- src/components/YoutubeVideo.tsx
+++ src/components/YoutubeVideo.tsx
@@ -1,15 +1,31 @@
-import { AspectRatio, Heading } from "@chakra-ui/react";
+import { AspectRatio, Center, Heading, Divider, Box } from "@chakra-ui/react";
 import { useLocation, useParams } from "react-router-dom";
+import YoutubeTab from "@components/Tabs/YoutubeTab";
+import { useEffect, useState, useRef } from "react";
 
 const YoutubeVideo = () => {
   const location = useLocation<{ title: string }>();
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+  const [iframeHeight, setIframeHeight] = useState<number>(0);
 
   const { title } = location.state;
   const { id } = useParams<{ id: string }>();
 
+  useEffect(() => {
+    if (iframeRef?.current) {
+      setIframeHeight(iframeRef.current.offsetHeight);
+    }
+  }, [iframeRef]);
+
   return (
     <>
-      <AspectRatio ratio={16 / 9}>
+      <AspectRatio
+        ratio={16 / 9}
+        pos="fixed"
+        w="100%"
+        zIndex={100}
+        ref={iframeRef}
+      >
         <iframe
           title="YouTube video player"
           src={`https://www.youtube.com/embed/${id}`}
@@ -18,9 +34,26 @@ const YoutubeVideo = () => {
         />
       </AspectRatio>
 
-      <Heading as="h6" size="sm">
-        {title}
-      </Heading>
+      <Center
+        p={2}
+        bg="gray.200"
+        pos="fixed"
+        w="100%"
+        zIndex={100}
+        mt={iframeHeight}
+      >
+        play_bar
+      </Center>
+
+      <Box pt={iframeHeight + 40}>
+        <Heading as="h6" size="sm" p={3}>
+          {title}
+        </Heading>
+
+        <Divider />
+
+        <YoutubeTab />
+      </Box>
     </>
   );
 };
