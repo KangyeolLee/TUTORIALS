80000a6dded9bbc729d2e02a4f3d24bbeef0ad1b
KG <acrkdduf@naver.com>
2021-06-24 23:16:26 +0900

    add goback btn & menu btn in the Header and Drawer to menu btn

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,73 @@
-import { Center } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
+import {
+  Flex,
+  Box,
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
+  Text,
+  VStack,
+  StackDivider,
+} from "@chakra-ui/react";
+import { Link, useHistory, useLocation } from "react-router-dom";
+import { HamburgerIcon, ArrowBackIcon } from "@chakra-ui/icons";
+import { useRef } from "react";
 
 const Header = () => {
+  const location = useLocation();
+  const history = useHistory();
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const drawerBtnRef = useRef(null);
+
   return (
-    <Center
+    <Flex
       pos="fixed"
       w="100%"
       zIndex={100}
       p={3}
       bg="purple.600"
       color="#fff"
+      justify="space-between"
+      align="center"
     >
+      <ArrowBackIcon
+        onClick={() => history.goBack()}
+        opacity={location.pathname !== "/" ? "1" : "0"}
+      />
+
       <Link to="/">영웅시대</Link>
-    </Center>
+
+      <Box ref={drawerBtnRef} onClick={onOpen}>
+        <HamburgerIcon />
+      </Box>
+
+      <Drawer
+        isOpen={isOpen}
+        placement="right"
+        onClose={onClose}
+        finalFocusRef={drawerBtnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton color="#fff" />
+          <DrawerHeader bg="purple.600" color="#fff">
+            안내
+          </DrawerHeader>
+
+          <DrawerBody>
+            <VStack align="stretch" spacing={4} divider={<StackDivider />}>
+              <Text>공지사항</Text>
+              <Text>공유하기</Text>
+              <Text>보관함</Text>
+            </VStack>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </Flex>
   );
 };
 
