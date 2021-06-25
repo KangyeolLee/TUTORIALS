bd877a88fb230bbc0ac11b91d5ec9c670ab69bbb
KG <acrkdduf@naver.com>
2021-06-26 00:26:28 +0900

    delete next folder

--- React/next/src/component/Footer.js
+++
@@ -1,9 +0,0 @@
-import { Divider } from 'semantic-ui-react'
-export default function Footer() {
-  return (
-    <>
-      <Divider />
-      <div>Copyright #KG#. All rights reserved.</div>
-    </>
-  )
-}
