30778930804ecb09c45f53289c281059f6dd5409
KG <acrkdduf@naver.com>
2021-06-27 22:00:18 +0900

    add comments and modularzie the whole function into smaller functions

--- main.js
+++ main.js
@@ -1,15 +1,52 @@
-const { body } = document;
+const $main = document.getElementById("main");
 const $table = document.createElement("table");
 const $result = document.createElement("div");
 
+let clickable = true;
 let turn = "O";
 const rows = [];
 
-const checkWinnerAndDraw = (target) => {
+for (let i = 0; i < 3; i++) {
+  const $tr = document.createElement("tr");
+  const cells = [];
+  for (let i = 0; i < 3; i++) {
+    const $td = document.createElement("td");
+    cells.push($td);
+    $tr.append($td);
+  }
+  rows.push(cells);
+  $table.append($tr);
+}
+
+$table.addEventListener("click", checkTurnCallback);
+
+$main.append($table);
+$main.append($result);
+
+// 게임의 승부를 판단하는 함수
+function checkWinnerAndDraw(target) {
   const rowIndex = target.parentNode.rowIndex;
   const cellIndex = target.cellIndex;
 
-  let hasWinner = false;
+  const hasWinner = checkHasWinner(rowIndex, cellIndex);
+  const draw = rows.flat().every((td) => td.textContent);
+
+  if (hasWinner) {
+    endGame();
+    return;
+  }
+
+  if (draw) {
+    drawGame();
+    return;
+  }
+
+  turn = turn === "O" ? "X" : "O";
+}
+
+// 승리조건 체크 함수
+function checkHasWinner(rowIndex, cellIndex) {
+  let hasWinner;
 
   // 가로줄 체크
   if (
@@ -42,25 +79,23 @@ const checkWinnerAndDraw = (target) => {
     hasWinner = true;
   }
 
-  let draw = rows.flat().every((td) => td.textContent);
+  return hasWinner;
+}
 
-  if (hasWinner) {
-    $result.textContent = `${turn}님의 승리!`;
-    $table.removeEventListener("click", checkTurnCallback);
-    return;
-  }
+// 승리자가 나온 경우 게임종료
+function endGame() {
+  $result.textContent = `${turn}님의 승리!`;
+  $table.removeEventListener("click", checkTurnCallback);
+}
 
-  if (draw) {
-    $result.textContent = `${turn}님의 승리!`;
-    $table.removeEventListener("click", checkTurnCallback);
-    return;
-  }
+// 비긴 경우 게임종료
+function drawGame() {
+  $result.textContent = `무승부!`;
+  $table.removeEventListener("click", checkTurnCallback);
+}
 
-  turn = turn === "O" ? "X" : "O";
-};
-
-let clickable = true;
-const checkTurnCallback = (event) => {
+// 클릭 이벤트 핸들러 콜백 함수 : 차례에 따라 장기말 배치 및 게임 진행
+function checkTurnCallback(event) {
   if (!clickable) return;
   if (event.target.textContent) return;
 
@@ -69,33 +104,22 @@ const checkTurnCallback = (event) => {
   // 승부 검사
   checkWinnerAndDraw(event.target);
 
-  // 컴퓨터와 대련 기능
   if (turn === "X") {
-    clickable = false;
-    setTimeout(() => {
-      const emptyCells = rows.flat().filter((v) => !v.textContent);
-      const randomCell =
-        emptyCells[Math.floor(Math.random() * emptyCells.length)];
-      randomCell.textContent = "X";
-      checkWinnerAndDraw(event.target);
-      clickable = true;
-    }, 1000);
+    handleTurnOfComputer();
   }
-};
-
-for (let i = 0; i < 3; i++) {
-  const $tr = document.createElement("tr");
-  const cells = [];
-  for (let i = 0; i < 3; i++) {
-    const $td = document.createElement("td");
-    cells.push($td);
-    $tr.append($td);
-  }
-  rows.push(cells);
-  $table.append($tr);
 }
 
-$table.addEventListener("click", checkTurnCallback);
+// 컴퓨터 대국 기능 함수
+function handleTurnOfComputer() {
+  clickable = false;
 
-body.append($table);
-body.append($result);
+  setTimeout(() => {
+    const emptyCells = rows.flat().filter((v) => !v.textContent);
+    const randomCell =
+      emptyCells[Math.floor(Math.random() * emptyCells.length)];
+    randomCell.textContent = "X";
+
+    checkWinnerAndDraw(randomCell);
+    clickable = true;
+  }, 1000);
+}
