bf7fdd77abc78f17156b5288efa9db39ed695b9f
KG <acrkdduf@naver.com>
2021-07-10 18:46:11 +0900

    Node 클래스 onClick 프로퍼티 추가
    - 각 Node는 유저 인터랙션에 따라 state가 변경되어 리렌더링 되어야 함
    - 기존 코드는 단순히 상태를 기준으로 렌더링하기 때문에 유저와의 상호작용 불가
    - 인터랙션이 발생하면 Nodes뿐만아니라 Breadcrumb에도 영향을 주게 됨
    - 이때 직접 Breadcrumb 요소에 접근하게 되면 의존성이 생기므로 독립적으로 Nodes 클래스 사용 불가
    - 의존성을 제거한 채 느슨한 결합을 위해 onClick 이벤트 콜백을 프로퍼티로 전달하여 사용

--- src/js/Nodes.js
+++ src/js/Nodes.js
@@ -1,6 +1,7 @@
 class Nodes {
-  constructor({ $app, initialState }) {
+  constructor({ $app, initialState, onClick }) {
     this.state = initialState;
+    this.onClick = onClick;
 
     this.$target = document.createElement('ul');
     $app.append(this.$target);
@@ -15,5 +16,33 @@ class Nodes {
 
   render() {
     this.$target.innerHTML = this.state.nodes.map(node => `<li>${node.name}</li>`);
+
+    if(this.state.nodes) {
+      const nodesTemplate = this.state.nodes.map(node => {
+        const path = node.type === "FILE" ? './assets/file.png' : './assets/directory.png';
+
+        return `
+          <div class="Node" data-node-id="${node.id}">
+            <img src="${path}" alt="현재위치 아이콘 이미지" />
+            <div>${node.name}</div>
+          </div>
+        `
+      }).join('');
+
+      this.$target.innerHTML = !this.state.isRoot 
+        ? `<div class="Node"><img src="./assets/prev.png" alt="뒤로가기 버튼"/></div>${nodesTemplate}`
+        : nodesTemplate; 
+    }
+
+    this.$target.querySelectorAll('.Node').forEach($node => {
+      $node.addEventListener('click', e => {
+        const { nodeId } = e.target.dataset;
+        const selectedNode = this.state.nodes.find(node => node.id === nodeId);
+
+        if (selectedNode) {
+          this.onCLick(selectedNode);
+        }
+      })
+    })
   }
 }
