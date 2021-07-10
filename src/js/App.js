e7b7d7474475472e7c218ca7e6f29132370f2c8f
KG <acrkdduf@naver.com>
2021-07-10 19:39:33 +0900

    App 컴포넌트 초기 렌더링 로직 설계
    - init 메서드 구현
    - api를 통해 필요한 데이터를 가져오고 이를 초기 state로 설정
    - 두 컴포넌트를 private 멤버변수로 관리하며 리렌더링 관리

--- src/js/App.js
+++ src/js/App.js
@@ -1,3 +1,7 @@
+import request from "./api";
+import Breadcrumb from "./BreadCrumb";
+import Nodes from "./Nodes";
+
 export default class App {
   constructor($app) {
     this.state = {
@@ -6,12 +10,19 @@ export default class App {
       depth: [],      // 상위 디렉토리에서 하위 디렉토리까지 이동 시 거쳐온 서브 디렉토리 
     }
 
-    const breadcurmb = new Breadcrumb({
+    // _variables는 클래스에서 private member를 의미하는 일종의 컨벤션
+    // 공식적으로 private 키워드를 지원하지 않기 때문에 사용하는 컨벤션으로 문법적으로 강제되지 않는 규약
+    // stage-3 단계에서 js class 에서도 private 접근제어자를 사용할 수 있는 문법(#)을 지원하고 있으나
+    // 일부 모던 브라우저를 제외하면 아직 정식으로 채택되지 않아 사용이 불가능하므로 여기서는 사용하지 않음
+    // 타입스크립트를 사용한다면 private 접근제어자 사용이 가능하나, 타입스크립트 역시 내부적으로 자바스크립트로 돌아가기 때문에
+    // 별도로 외부에서 접근이 불가능하도록 변환한다.
+
+    this._breadcurmb = new Breadcrumb({
       $app,
       initialState: this.state.depth
     });
 
-    const nodes = new Nodes({
+    this._nodes = new Nodes({
       $app,
       initialState: {
         isRoot: this.state.isRoot,
@@ -25,5 +36,29 @@ export default class App {
         }
       }
     });
+
+    this.init();
+  }
+
+  setState(nextState) {
+    this.state = nextState;
+    this._breadcurmb.setState(this.state.depth);
+    this._nodes.setState({
+      isRoot: this.state.isRoot,
+      nodes: this.state.nodes,
+    });
+  }
+
+  async init() {
+    try {
+      const rootNodes = await request();
+      this.setState({
+        ...this.state,
+        isRoot: true,
+        nodes: rootNodes,
+      });
+    } catch (error) {
+      throw new Error(`통신 중 에러가 발생했습니다: ${error.message}`);
+    }
   }
 }
