4a622bb46342c8692dc9fc1ee2441995aeeb81a8
KG <acrkdduf@naver.com>
2021-07-10 18:59:00 +0900

    두 컴포넌트를 상위에서 조욜할 App 컴포넌트 작성
    - Breadcrumb 컴포넌트와 Nodes 컴포넌트는 추상화 되어 각각 독립적으로 작동해야 함
    - 이들간의 의존성을 최소화하기 위해 각각의 컴포넌트를 상위에서 조율할 별도의 컴포넌트 App 작성
    - 상위의 컴포넌트를 통해 각 컴포넌트들은 느슨한 결합을 유지하고 재사용성을 최대한 유지 가능
    - app 컴포넌트의 기본 골격(skeleton)만 우선 작성

---
+++ src/js/App.js
@@ -0,0 +1,29 @@
+class App {
+  constructor($app) {
+    this.state = {
+      isRoot: false,  // 현재 페이지가 메인페이지인지 판단하는 플래그
+      nodes: [],      // 현재 화면에 따라 출력해야할 nodes 데이터
+      depth: [],      // 상위 디렉토리에서 하위 디렉토리까지 이동 시 거쳐온 서브 디렉토리 
+    }
+
+    const breadcurmb = new Breadcrumb({
+      $app,
+      initialState: this.state.depth
+    });
+
+    const nodes = new Nodes({
+      $app,
+      initialState: {
+        isRoot: this.state.isRoot,
+        nodes: this.state.nodes,
+      },
+      onClick: (node) => {
+        if (node.type === 'DIRECTORY') {
+          // Node의 타입이 디렉토리인 경우 넘겨주는 콜백 작성
+        } else if (node.type === 'FILE') {
+          // Node의 타입이 파일인 경우 넘겨주는 콜백 작성
+        }
+      }
+    });
+  }
+}
