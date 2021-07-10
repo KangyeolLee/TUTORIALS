00b3fac3b0f7b41bb9624ec0ae2b07740382f6f7
KG <acrkdduf@naver.com>
2021-07-10 18:25:27 +0900

    컴포넌트 단위 추상화와 재사용성을 위한 클래스 Breadcrumb 작성
    - 기본 골격 작성
    - 내부 상태(state) 값에 따라 렌더링
    - DOM에 접근하고 업데이트 하는 시점에 대한 명확한 기준 제시 : 인스턴스 생성시 + 상태값 변경시

---
+++ src/js/Breadcrumb.js
@@ -0,0 +1,22 @@
+class Breadcrumb {
+  constructor({ $app, initialState }) {
+    this.state = initialState;
+        
+    this.$target = document.createElement('nav');
+    this.$target.classList.add('Breadcrumb');
+    $app.append(this.$target);
+  }
+
+  setState(nextState) {
+    this.state = nextState;
+    this.render();
+  }
+
+  render() {
+    this.$target.innerHTML = `<div class="nav-item">root</div>${
+      this.state.map(
+        (node, index) => `<div class="nav-item" data-indx="${index}">${node.name}</div>`
+      ).join('')
+    }`;
+  }
+}
