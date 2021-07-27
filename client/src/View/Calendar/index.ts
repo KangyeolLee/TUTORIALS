c4f52a3ef6955d88e206fcb3f926cc33f256f485
KG <acrkdduf@naver.com>
2021-07-28 04:08:50 +0900

    [#4] feat: 불필요한 파일 제거

--- client/src/View/Calendar/index.ts
+++
@@ -1,8 +0,0 @@
-import Component from '@/Core/Component';
-import { html } from '@/utils/helper';
-
-export default class Calendar extends Component {
-  template() {
-    return html`<div>e달력</div>`;
-  }
-}
