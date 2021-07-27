a9c95a31b7018aba89f660a1c0725bb90867423c
KG <acrkdduf@naver.com>
2021-07-27 12:31:36 +0900

    feat: 메인:내역 / 달력 / 차트 페이지 라우터 추가

---
+++ client/src/View/Main/index.ts
@@ -0,0 +1,8 @@
+import Component from '@/Core/Component';
+import { html } from '@/utils/helper';
+
+export default class Main extends Component {
+  template() {
+    return html`<div>메인</div>`;
+  }
+}
