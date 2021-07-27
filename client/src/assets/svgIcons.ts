67c99a9e40530de82beb698cf29a1f2854460c36
KG <acrkdduf@naver.com>
2021-07-27 18:18:07 +0900

    [#1] refactor:  파일 형식 내보내기 설정 주석처리 (추후 확장성을 위해 잔존)

---
+++ client/src/assets/svgIcons.ts
@@ -0,0 +1,27 @@
+export const svgIcons = {
+  fileText: `<svg width="18" height="22" viewBox="0 0 18 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M11 1H3C2.46957 1 1.96086 1.21071 1.58579 1.58579C1.21071 1.96086 1 2.46957 1 3V19C1 19.5304 1.21071 20.0391 1.58579 20.4142C1.96086 20.7893 2.46957 21 3 21H15C15.5304 21 16.0391 20.7893 16.4142 20.4142C16.7893 20.0391 17 19.5304 17 19V7L11 1Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M11 1V7H17"  stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M13 12H5"  stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M13 16H5"  stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M7 8H6H5"  stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
+  `,
+  calendar: `<svg width="20" height="22" viewBox="0 0 20 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M17 3H3C1.89543 3 1 3.89543 1 5V19C1 20.1046 1.89543 21 3 21H17C18.1046 21 19 20.1046 19 19V5C19 3.89543 18.1046 3 17 3Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M14 1V5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M6 1V5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M1 9H19" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
+  `,
+  chart: `<svg width="14" height="18" viewBox="0 0 14 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M13 17V7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M7 17V1" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M1 17V11" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+    `,
+  check: `<svg width="20" height="14" viewBox="0 0 20 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M19 1L6.625 13L1 7.54545" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+    `,
+};
