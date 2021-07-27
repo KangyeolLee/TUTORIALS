2511373b7cc707e6d67e388cd2b2b389522c5970
KG <acrkdduf@naver.com>
2021-07-27 12:28:54 +0900

    feat: Header 컴포넌트 기본 구조 작성

---
+++ client/src/Components/Header/index.ts
@@ -0,0 +1,26 @@
+import Component from '@/Core/Component';
+import { html } from '@/utils/helper';
+import { $router } from '@/Core/Router';
+
+export default class Header extends Component {
+  template() {
+    return html`
+      <div>
+        헤더입니당
+        <button class="main">메인</button>
+        <button class="calendar">달력</button>
+        <button class="chart">차트</button>
+      </div>
+    `;
+  }
+
+  mounted() {
+    const $main = this.$target.querySelector('.main') as HTMLElement;
+    const $calendar = this.$target.querySelector('.calendar') as HTMLElement;
+    const $chart = this.$target.querySelector('.chart') as HTMLElement;
+
+    $main.addEventListener('click', () => $router.push('/main'));
+    $calendar.addEventListener('click', () => $router.push('/calendar'));
+    $chart.addEventListener('click', () => $router.push('/charts'));
+  }
+}
