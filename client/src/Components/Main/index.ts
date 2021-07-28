ad8585651b60308189085e6e4b49e4c743361c23
eunseonglee <eunseongofficial@gmail.com>
2021-07-28 22:12:49 +0900

    [#5] feat: 헤더의 달에 해당하는 history 데이터 가져오기
    1. DateModel을 구독하여 달을 가져옵니다.
    2. dummyData가 월이 섞여 있어 필터링을 넣었습니다.. 만 DB에서 가져올 때 필터링하면 되니 나중에 해당 부분 삭제해도 될 것같습니다.

--- client/src/Components/Main/index.ts
+++ client/src/Components/Main/index.ts
@@ -1,24 +1,33 @@
 import Component from '@/Core/Component';
 import './styles';
-import { Props, State } from '@/utils/types';
+import { Props, State, Today } from '@/utils/types';
 import { html } from '@/utils/helper';
 import MainModel from '@/Model/MainModel';
 import HistoryDayCard from '../HistoryDayCard';
 import { IHistory } from '@/utils/types';
+import DateModel from '@/Model/DateModel';
 
 interface IMainState extends State {
   historyCards: IHistory[];
+  today: Today;
 }
 
 export default class Main extends Component<IMainState, Props> {
-  model: any;
+  mainModel: any;
+  dateModel: any;
 
   setup() {
-    this.model = MainModel;
-    this.model.subscribe(this.model.key, this);
+    // main 모델(history) 구독
+    this.mainModel = MainModel;
+    // date 모델 구독
+    this.dateModel = DateModel;
+    this.dateModel.subscribe(this.dateModel.key, this);
+
     this.$state = {
-      historyCards: this.model.historyCards,
+      historyCards: this.mainModel.historyCards,
+      today: this.dateModel.today,
     };
+    console.log(this.$state.today);
   }
 
   template() {
@@ -26,15 +35,20 @@ export default class Main extends Component<IMainState, Props> {
   }
 
   mounted() {
-    const { historyCards } = this.$state!; // '!' 지우고 싶어요...
+    const { historyCards, today } = this.$state!;
 
     const $daycardList = this.$target.querySelector(
       '.day-card-list'
     ) as HTMLElement;
 
-    const historyDates = historyCards.map(
-      (history) => history.date
-    ) as string[];
+    const historyDates = historyCards
+      .filter((history) => {
+        const [year, month, _] = history.date
+          .split('-')
+          .map((d) => parseInt(d));
+        return today.year === year && today.month === month;
+      })
+      .map((history) => history.date) as string[];
     const dates = Array.from(new Set(historyDates)).sort().reverse();
 
     dates.forEach((date) => {
@@ -46,5 +60,10 @@ export default class Main extends Component<IMainState, Props> {
 
       $daycardList.appendChild($li);
     });
+  }
+
+  setUnmount() {
+    this.mainModel.unsubscribe(this.mainModel.key, this);
+    this.dateModel.unsubscribe(this.dateModel.key, this);
   }
 }
