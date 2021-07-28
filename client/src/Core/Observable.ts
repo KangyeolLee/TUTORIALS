ad7b4f90867e3b7d7e1258ab71d2d6b0d0cd07f8
KG <acrkdduf@naver.com>
2021-07-28 16:25:38 +0900

    [#4] feat: 중복검사 로직 제외 및 타입 추가

--- client/src/Core/Observable.ts
+++ client/src/Core/Observable.ts
@@ -1,25 +1,18 @@
 /* any는 나중에 꼭꼮꼭 타입을 바꿔주도록 해보아요 */
-import { Duplicates, Store } from '@/Core/Store';
+import { Store } from '@/Core/Store';
 
 export default class Observable {
   private _observers: any;
-  private _duplicate: any;
 
   constructor() {
     this._observers = Store.getStates;
-    this._duplicate = Duplicates.getStates;
   }
 
-  subscribe(key: string, observer_name: string, observer: any) {
+  subscribe(key: string, observer: any) {
     if (!this._observers[key]) {
       this._observers[key] = [];
     }
 
-    if (this._duplicate.has(observer_name)) {
-      this.unsubscribe(key, observer);
-    }
-
-    this._duplicate.add(observer_name);
     this._observers[key].push(observer);
   }
 
