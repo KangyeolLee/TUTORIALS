cc9cb87c2ce7e3922956b85938ee58d620c8d020
KG <acrkdduf@naver.com>
2021-07-29 15:38:15 +0900

    [#30] chore: default export 값 재설정

--- server/loaders/index.ts
+++ server/loaders/index.ts
@@ -1,6 +1,12 @@
 import express from 'express';
 import expressLoader from './express.loader';
+import databaseLoader from './database.loader';
 
 export default async ({ app }: { app: express.Application }) => {
-  await expressLoader({ app });
+  try {
+    await databaseLoader.connect();
+    await expressLoader({ app });
+  } catch (error) {
+    throw new Error('[로더 에러] : ' + error);
+  }
 };
