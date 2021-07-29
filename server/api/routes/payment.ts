bf6b744b870a81c15222326f5f437dc0e5644949
KG <acrkdduf@naver.com>
2021-07-30 05:05:35 +0900

    [#35] feat: 결제수단 라우트 경로 작성

--- server/api/routes/payment.ts
+++ server/api/routes/payment.ts
@@ -1,37 +1,14 @@
-import { Router, Request, Response, NextFunction } from 'express';
+import { Router } from 'express';
+import paymentController from '../../controllers/payment.controller';
 
 const router = Router();
 
 export default (app: Router) => {
   app.use('/payments', router);
 
-  router.get(`/`, (req: Request, res: Response, next: NextFunction) => {
-    return res
-      .json({
-        message: '성공적으로 payment를 가져왔습니다.',
-        ok: true,
-      })
-      .status(200);
-  });
+  router.get(`/`, paymentController.findPayments);
 
-  router.post(`/`, (req: Request, res: Response, next: NextFunction) => {
-    return res
-      .json({
-        message: '성공적으로 payment를 추가했습니다.',
-        ok: true,
-      })
-      .status(200);
-  });
+  router.post(`/`, paymentController.createPayment);
 
-  router.delete(
-    `/:paymentId`,
-    (req: Request, res: Response, next: NextFunction) => {
-      return res
-        .json({
-          message: '성공적으로 payment를 삭제했습니다.',
-          ok: true,
-        })
-        .status(200);
-    }
-  );
+  router.delete(`/:paymentId`, paymentController.deleteCategory);
 };
