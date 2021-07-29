943ba295d4564ea6f6dae32cb5c379e43b3550f4
KG <acrkdduf@naver.com>
2021-07-30 02:30:31 +0900

    [#37] feat: 카테고리 서비스 로직 작성

--- server/services/category.services.ts
+++ server/services/category.services.ts
@@ -1,17 +1,49 @@
 import { getCustomRepository } from 'typeorm';
-import CategoryRepository from './../repositories/category.repository';
+import {
+  CategoryRepository,
+  UserCategoryRepository,
+} from '../repositories/category.repository';
 
 class CategoryService {
   async findCategories(userId: number) {
     try {
-      console.log('hello world');
       const categories = await getCustomRepository(
-        CategoryRepository
+        UserCategoryRepository
       ).findAllByUserId(userId);
-      console.log(categories);
       return categories;
     } catch (error) {
       throw new Error('[카테고리 쿼리 에러] ' + error);
+    }
+  }
+
+  async createCategory(userId: number, type: string, color: string) {
+    try {
+      const category = await getCustomRepository(
+        CategoryRepository
+      ).createCategoryForUser(type);
+      const {
+        raw: { insertId },
+      } = category!;
+
+      const result = await getCustomRepository(
+        UserCategoryRepository
+      ).createUserCategoryByUserId(userId, insertId, color);
+
+      return result;
+    } catch (error) {
+      throw new Error('[카테고리 쿼리 에러] ' + error);
+    }
+  }
+
+  async deleteUserCategoryByUserId(userId: number, id: number) {
+    try {
+      const result = await getCustomRepository(
+        UserCategoryRepository
+      ).deleteUserCategoryByUserId(userId, id);
+
+      return result;
+    } catch (error) {
+      throw new Error('[카테고리 쿼리 에러]' + error);
     }
   }
 }
