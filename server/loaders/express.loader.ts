6c53fb4298eaa9efded89fd0ce44c4d938468acd
KG <acrkdduf@naver.com>
2021-08-02 02:33:27 +0900

    [#53] feat: express cors 설정 (추후 배포서버 수정 필요)
    Co-authored-by: KangyeolLee <acrkdduf@naver.com>
    Co-authored-by: eun-seong <eunseongofficial@gmail.com>

--- server/loaders/express.loader.ts
+++ server/loaders/express.loader.ts
@@ -1,6 +1,14 @@
 import express, { NextFunction, Request, Response } from 'express';
 import config from '../config';
 import routes from '../api';
+import cors from 'cors';
+
+const options = {
+  origin: ['http://localhost:8080'], // 접근 권한을 부여하는 도메인
+  methods: ['POST', 'GET', 'DELETE', 'PUT'],
+  credentials: true, // 응답 헤더에 Access-Control-Allow-Credentials 추가
+  optionsSuccessStatus: 200, // 응답 상태 200으로 설정
+};
 
 export default ({ app }: { app: express.Application }) => {
   app.set('port', config.port || 3000);
@@ -8,6 +16,7 @@ export default ({ app }: { app: express.Application }) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
 
+  app.use(cors(options));
   app.use(config.api, routes());
 
   app.use((req: Request, res: Response, next: NextFunction) => {
