4453900be293bdced2122c51455d3021a42dc2a0
KG <acrkdduf@naver.com>
2021-08-02 00:12:25 +0900

    [#51] feat: api routes 경로에 미들웨어 적용
    Co-authored-by: KangyeolLee <acrkdduf@naver.com>
    Co-authored-by: eun-seong <eunseongofficial@gmail.com>

--- server/api/routes/payment.ts
+++ server/api/routes/payment.ts
@@ -1,14 +1,19 @@
 import { Router } from 'express';
-import paymentController from '../../controllers/payment.controller';
+import PaymentController from '../../controllers/payment.controller';
+import { authMiddleware } from '../middlewares/auth.middleware';
 
 const router = Router();
 
 export default (app: Router) => {
   app.use('/payments', router);
 
-  router.get(`/`, paymentController.findPayments);
+  router.get(`/`, authMiddleware, PaymentController.findPayments);
 
-  router.post(`/`, paymentController.createPayment);
+  router.post(`/`, authMiddleware, PaymentController.createPayment);
 
-  router.delete(`/:paymentId`, paymentController.deleteCategory);
+  router.delete(
+    `/:paymentId`,
+    authMiddleware,
+    PaymentController.deleteCategory
+  );
 };
