550a701783ef8f9a2fedf034220145385187e107
KG <acrkdduf@naver.com>
2021-08-02 00:13:34 +0900

    [#51] feat: controller에 request로 부터 유저 정보 획득하도록 변경
    Co-authored-by: KangyeolLee <acrkdduf@naver.com>
    Co-authored-by: eun-seong <eunseongofficial@gmail.com>

--- server/controllers/category.controller.ts
+++ server/controllers/category.controller.ts
@@ -1,12 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
 import categoryServices from '../services/category.services';
 import { ResultRawType } from '../types/types';
+import { getPayload } from './../utils/getPayload';
 
 class CategoryController {
   async findCategories(req: Request, res: Response, next: NextFunction) {
     try {
       // userId 로그인 세션에서 가져와야 함!!
-      const userId = 1;
+      const userId = getPayload(req);
+      console.log(userId);
+
       const categories = await categoryServices.findCategories(userId);
 
       return res.status(200).json({
@@ -21,7 +24,8 @@ class CategoryController {
   async createCategory(req: Request, res: Response, next: NextFunction) {
     try {
       // userId 로그인 세션에서 가져와야 함!!
-      const userId = 1;
+      const userId = getPayload(req);
+
       const { type, color } = req.body;
       const result = await categoryServices.createCategory({
         userId,
@@ -44,8 +48,7 @@ class CategoryController {
 
   async deleteCategory(req: Request, res: Response, next: NextFunction) {
     try {
-      // userId 로그인 세션에서 가져와야 함!!
-      const userId = 1;
+      const userId = getPayload(req);
       // 선택한 카테고리의 고유 id 값을 의미
       const { categoryId } = req.params;
       const result = await categoryServices.deleteUserCategoryByUserId({
--- server/controllers/payment.controller.ts
+++ server/controllers/payment.controller.ts
@@ -1,12 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
 import paymentServices from '../services/payment.services';
 import { ResultRawType } from '../types/types';
+import { getPayload } from '../utils/getPayload';
 
 class PaymentController {
   async findPayments(req: Request, res: Response, next: NextFunction) {
     try {
       // userId 로그인 세션에서 가져와야 함!!
-      const userId = 1;
+      const userId = getPayload(req);
       const payments = await paymentServices.findPayments(userId);
 
       return res.status(200).json({
@@ -21,7 +22,7 @@ class PaymentController {
   async createPayment(req: Request, res: Response, next: NextFunction) {
     try {
       // userId 로그인 세션에서 가져와야 함!!
-      const userId = 1;
+      const userId = getPayload(req);
       const { type } = req.body;
       const result = await paymentServices.createPayment({ userId, type });
       const {
@@ -40,8 +41,7 @@ class PaymentController {
 
   async deleteCategory(req: Request, res: Response, next: NextFunction) {
     try {
-      // userId 로그인 세션에서 가져와야 함!!
-      const userId = 1;
+      const userId = getPayload(req);
       // 선택한 카테고리의 고유 id 값을 의미
       const { paymentId } = req.params;
       const result = await paymentServices.deleteUserPaymentByUserId({
