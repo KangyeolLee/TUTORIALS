d457667e392dd368a746730cf92ad82609b94bd6
eunseonglee <eunseongofficial@gmail.com>
2021-08-02 22:29:43 +0900

    [#59] feat: UserView 페이지 마크업

--- client/src/View/UserView/index.ts
+++ client/src/View/UserView/index.ts
@@ -3,16 +3,51 @@ import Component from '@/Core/Component';
 import { html } from '@/utils/helper';
 import { Props, State } from '@/utils/types';
 import { svgIcons } from '@/assets/svgIcons';
+import User from '@/Components/User';
+import { getUserInfo } from '@/api/user';
 
-export default class UserView extends Component<State, Props> {
+interface UserViewState extends State {
+  id: number;
+  githubId: string;
+}
+
+export default class UserView extends Component<UserViewState, Props> {
+  setup() {
+    getUserInfo()
+      .then((res) => {
+        this.setState({
+          id: res.data.id,
+          githubId: res.data.githubId,
+        });
+      })
+      .catch((err) => {
+        // TODO 로그인 실패 알림
+        console.error(err);
+      });
+  }
+
   template() {
+    // 로딩 중
+    if (!this.$state!.hasOwnProperty('id')) return html`Loading...`;
+
     return html`
       <section class="user-wrapper container">
-        <button class="login-btn">
-          ${svgIcons.github}
-          <span>Github로 로그인</span>
-        </button>
+        <a href="http://${location.hostname}:3000/api/user/login">
+          <button class="github-btn">
+            ${svgIcons.github}
+            <span>Github로 로그인</span>
+          </button>
+        </a>
       </section>
     `;
+  }
+
+  mounted() {
+    if (this.$state!.id !== 1) {
+      const $userWrapper = this.$target.querySelector(
+        '.user-wrapper'
+      ) as HTMLElement;
+      new User($userWrapper, { user: this.$state });
+    }
   }
 }
