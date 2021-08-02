7dd69c6c5214b5d1cef29965240d0ed0a3225b0a
KG <acrkdduf@naver.com>
2021-08-03 03:03:56 +0900

    [#61] refactor: HistoryList 및 CategoryTag 컴포넌트 함수형으로 변경
    컴포넌트 클래스 형으로는 너무 오버 엔지니어링으로 판단했습니다.

--- client/src/Components/CategoryTag/index.ts
+++ client/src/Components/CategoryTag/index.ts
@@ -1,25 +1,15 @@
-import Component from '@/Core/Component';
 import './styles';
-import { Props, State } from '@/utils/types';
-import { html } from '@/utils/helper';
 import { category } from '@/assets/dummy';
 
-interface ICategoryTagStates extends State {
-  type: string;
-}
-
-export default class CategoryTag extends Component<ICategoryTagStates, Props> {
-  template() {
-    const { type } = this.$state!;
-    const isExisting = category.filter((c) => c.type === type)[0];
-    return html`
-      <div
-        class="category-tag"
-        data-id="${isExisting ? isExisting.id : category[6].id}"
-      >
-        ${isExisting ? isExisting.type : category[6].type}
-      </div>
-    `;
-  }
-  removeEvent() {}
+export default function CategoryTag(categoryType: string) {
+  const type = categoryType;
+  const isExisting = category.filter((c) => c.type === type)[0];
+  return `
+    <div
+      class="category-tag"
+      data-id="${isExisting ? isExisting.id : category[6].id}"
+    >
+      ${isExisting ? isExisting.type : category[6].type}
+    </div>
+  `;
 }
