d2aeae1db8eea73b659c6c86a115ffc8c3feebff
eunseonglee <eunseongofficial@gmail.com>
2021-08-03 02:09:13 +0900

    [#59] feat: user 컴포넌트 마크업 수정

--- client/src/Components/User/index.ts
+++ client/src/Components/User/index.ts
@@ -4,14 +4,48 @@ import { Props, State } from '@/utils/types';
 import { html } from '@/utils/helper';
 import { apiLogout } from '@/api/auth';
 import { $router } from '@/Core/Router';
+import CategoryIcon from '@/Components/CategoryIcon';
 
-export default class User extends Component<State, Props> {
+interface UserState extends State {
+  user: {
+    id: number;
+    githubUser: string;
+  };
+}
+
+export default class User extends Component<UserState, Props> {
   template() {
-    return html` <button class="user-btn" id="logout-btn">로그아웃</button> `;
+    return html`
+      <div class="user">
+        <section class="content-box user-info">
+          <span class="github-user">${this.$state!.user.githubUser}</span>
+          <button class="logout-btn">로그아웃</button>
+        </section>
+        <section class="content-box user-payments">
+          <section class="content-box-title user-category-title">
+            <span class="title">카테고리</span>
+            <span class="edit-button">편집</span>
+          </section>
+          <ul class="user-payments-icons"></ul>
+        </section>
+      </div>
+    `;
+  }
+
+  mounted() {
+    const $userPaymentsIcons = this.$target.querySelector(
+      '.user-payments-icons'
+    ) as HTMLElement;
+
+    for (let i = 0; i < 8; i++) {
+      const $li = document.createElement('li');
+      new CategoryIcon($li);
+      $userPaymentsIcons.appendChild($li);
+    }
   }
 
   setEvent() {
-    this.addEvent('click', '#logout-btn', this.handleLogOut);
+    this.addEvent('click', '.logout-btn', this.handleLogOut);
   }
 
   async handleLogOut() {
