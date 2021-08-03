43e647996413c365f21213fc7c9b5660c9802b65
eunseonglee <eunseongofficial@gmail.com>
2021-08-03 17:38:03 +0900

    [#59] feat: payment 프론트 API 작성

--- client/src/api/payment.ts
+++ client/src/api/payment.ts
@@ -5,10 +5,14 @@ const APIENDPOINT = `http://localhost:3000/api/payments`;
 export const getPayments = async () =>
   await axios.get(`${APIENDPOINT}`, { withCredentials: true });
 
-export const createPayments = async (type: string) =>
-  await axios.post(`${APIENDPOINT}`, {
-    type,
-  });
+export const createPayment = async (type: string) =>
+  await axios.post(
+    `${APIENDPOINT}`,
+    {
+      type,
+    },
+    { withCredentials: true }
+  );
 
-export const deletePayments = async (categoryId: number) =>
-  await axios.delete(`${APIENDPOINT}/${categoryId}`);
+export const deletePayment = async (categoryId: number) =>
+  await axios.delete(`${APIENDPOINT}/${categoryId}`, { withCredentials: true });
