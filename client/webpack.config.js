66a554f483d4148a86a2a2eb6d9fbbc8f8616c82
KG <acrkdduf@naver.com>
2021-08-05 20:52:53 +0900

    [#92] feat: ENV 파일 처리를 위한 웹팩 설정

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,73 +1,79 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
 const path = require('path');
+const dotenv = require('dotenv-webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/main.ts',
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)$/,
-        use: {
-          loader: 'babel-loader',
+module.exports = (env) => {
+  console.log(env);
+  return {
+    entry: './src/main.ts',
+    module: {
+      rules: [
+        {
+          test: /\.(ts|js)$/,
+          use: {
+            loader: 'babel-loader',
+          },
+          exclude: ['/node_modules'],
         },
-        exclude: ['/node_modules'],
-      },
-      {
-        test: /\.(css|scss)$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          {
-            loader: 'sass-loader',
+        {
+          test: /\.(css|scss)$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            {
+              loader: 'sass-loader',
+              options: {
+                implementation: require('sass'), //dart-sass 적용
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          use: {
+            loader: 'url-loader',
             options: {
-              implementation: require('sass'), //dart-sass 적용
+              esModule: false,
+              limit: 8192,
+              name: 'assets/[name]?[hash].[ext]',
             },
           },
-        ],
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        use: {
-          loader: 'url-loader',
-          options: {
-            esModule: false,
-            limit: 8192,
-            name: 'assets/[name]?[hash].[ext]',
-          },
         },
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ template: './src/index.html' }),
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css',
-    }),
-    new CleanWebpackPlugin(),
-  ],
-  optimization: { minimize: true },
-  output: {
-    publicPath: '/',
-    path: path.join(__dirname, './dist'),
-    filename: '[name].js',
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: path.join(__dirname, 'dist'),
-    publicPath: '/',
-  },
-  resolve: {
-    modules: ['node_modules'],
-    extensions: ['.ts', '.js', '.json', '.scss'],
-    alias: {
-      '@': path.join(__dirname, 'src'),
+      ],
     },
-  },
-  devtool: 'source-map',
+    plugins: [
+      new HtmlWebpackPlugin({ template: './src/index.html' }),
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+        chunkFilename: '[id].css',
+      }),
+      new CleanWebpackPlugin(),
+      new dotenv({
+        path: env.production ? '../.env.prod' : '../.env.dev',
+      }),
+    ],
+    optimization: { minimize: true },
+    output: {
+      publicPath: '/',
+      path: path.join(__dirname, './dist'),
+      filename: '[name].js',
+    },
+    devServer: {
+      historyApiFallback: true,
+      contentBase: path.join(__dirname, 'dist'),
+      publicPath: '/',
+    },
+    resolve: {
+      modules: ['node_modules'],
+      extensions: ['.ts', '.js', '.json', '.scss'],
+      alias: {
+        '@': path.join(__dirname, 'src'),
+      },
+    },
+    devtool: 'source-map',
+  };
 };
